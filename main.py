5f8f2bb1886192bf7d812eacb17a6eb5fbc50dee
StivenPeter <stivenpeter@gmail.com>
2017-04-24 22:36:05 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def intersect(a,b):
+    return [i for i in a if i in b]
+def union(a,b): 
+    return a + diff(b,a)
+def diff(a, b):
+    return [i for i in a if i not in b]
+def symmetricdiff(a,b):
+    return diff(union(a,b),intersect(a,b))
+def prod(a,b):   
+    reutnr [(i, j) for i in a for j in b]
